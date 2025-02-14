b2dd2a4c4bdfe79af15ebfbf64f4b1c0b81e93d4
JLipreso <lipreso.developer@gmail.com>
2025-02-14 15:43:54 +0800

    OK

--- index.ts
+++ index.ts
@@ -5,7 +5,6 @@ export * from './query/fetchSingle';
 export * from './query/fetchPaginate';
 export * from './query/insertGetID';
 export * from './query/update';
-
 export * from './utility/console';
 export * from './utility/currency';
 export * from './utility/format';
@@ -16,13 +15,12 @@ export * from './utility/reference_id';
 export * from './utility/token';
 export * from './config';
 export * from './message';
-
 export * from './users_admin/authentication';
 export * from './users_admin/fetchPaginate';
-
 export * from './users_student/authentication';
 export * from './users_student/fetchPaginate';
-
 export * from './video_group/fetchAll';
-
+export * from './video_tutorial/delete';
 export * from './video_tutorial/create';
+export * from './video_tutorial/fetchPaginate';
+export * from './video_tutorial/update';
